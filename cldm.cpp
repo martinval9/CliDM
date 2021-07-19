0ac02b1eded24693399b0824813153cb49c3026b
martinval9 <68918892+martinval9@users.noreply.github.com>
2021-07-19 13:25:26 -0300

    Update cldm.cpp

--- cldm.cpp
+++ cldm.cpp
@@ -4,7 +4,7 @@
 //Colores
 
 #define RESET_COLOR "\x1b[0m"
-#define RED_T      "\x1b[31m"
+#define RED_T       "\x1b[31m"
 #define CYAN_T      "\x1b[36m"
 
 using namespace std;
@@ -26,45 +26,58 @@ int main() {
         cout << "\nCldm" << CYAN_T << ": " << RESET_COLOR;
         cin >> decision;
 
+        FILE *file;
+        if (file = fopen("a.txt", "r")) {
+            fclose(file);
+        } else {} 
+
         // pkill cldm para evitar un consumo excesivo de cpu
-        if(decision == "dwm") {
-            system("startx /usr/bin/dwm && pkill cldm");
-        } else if (decision == "fluxbox") {
-            system("startx /usr/bin/fluxbox && pkill cldm");
-        } else if (decision == "lxde") {
-            system("startx /usr/bin/lxde && pkill cldm");
-        } else if (decision == "bspwm") {
-            system("startx /usr/bin/bspwm && pkill cldm");
-        } else if (decision == "xfce") {
-            system("startx /usr/bin/xfce && pkill cldm");
-        } else if (decision == "kde") {
-            system("startx /usr/bin/kde && pkill cldm");
-        } else if (decision == "gnome") {
-            system("startx /usr/bin/gnome && pkill cldm");
-        } else if (decision == "openbox") {
-            system("startx /usr/bin/openbox && pkill cldm");
-        } else if (decision == "herbstluftwm") {
-            system("startx /usr/bin/herbstluftwm && pkill cldm");
-        } else if (decision == "dk") {
-            system("startx /usr/bin/dk && pkill cldm");
-        } else if (decision == "gwm") {
-            system("startx /usr/bin/gwm && pkill cldm");
-        } else if (decision == "lxqt") {
-            system("startx /usr/bin/lxqt");
-        } else if (decision == "awesome") {
-            system("startx /usr/bin/awesome && pkill cldm");
-        } else if (decision == "cinnamon") {
-            system("startx /usr/bin/cinnamon && pkill cldm");
-        } else if (decision == "budgie") {
-            system("startx /usr/bin/budgie && pkill cldm");
-        } else if (decision == "pantheon") {
-            system("startx /usr/bin/pantheon && pkill cldm");
-        } else if (decision == "unity") {
-            system("startx /usr/bin/unity && pkill cldm");
-        } else if (decision == "deepin") {
-            system("startx /usr/bin/deepin && pkill cldm");
-        } else if (decision == "i3") {
-            system("startx /usr/bin/i3 && pkill cldm");
-        }
+        if(decision == "dwm") {system("startx /usr/bin/dwm && pkill cldm");} 
+
+        else if (decision == "fluxbox")      {system("startx /usr/bin/fluxbox && pkill cldm");} 
+
+        else if (decision == "lxde")         {system("startx /usr/bin/lxde && pkill cldm");} 
+
+        else if (decision == "bspwm")        {system("startx /usr/bin/bspwm && pkill cldm");} 
+
+        else if (decision == "xfce")         {system("startx /usr/bin/xfce && pkill cldm");} 
+
+        else if (decision == "kde")          {system("startx /usr/bin/kde && pkill cldm");} 
+
+        else if (decision == "gnome")        {system("startx /usr/bin/gnome && pkill cldm");} 
+
+        else if (decision == "openbox")      {system("startx /usr/bin/openbox && pkill cldm");} 
+
+        else if (decision == "herbstluftwm") {system("startx /usr/bin/herbstluftwm && pkill cldm");} 
+
+        else if (decision == "dk")           {system("startx /usr/bin/dk && pkill cldm");} 
+
+        else if (decision == "gwm")          {system("startx /usr/bin/gwm && pkill cldm");} 
+
+        else if (decision == "lxqt")         {system("startx /usr/bin/lxqt");} 
+
+        else if (decision == "awesome")      {system("startx /usr/bin/awesome && pkill cldm");} 
+
+        else if (decision == "cinnamon")     {system("startx /usr/bin/cinnamon && pkill cldm");} 
+
+        else if (decision == "budgie")       {system("startx /usr/bin/budgie && pkill cldm");} 
+
+        else if (decision == "pantheon")     {system("startx /usr/bin/pantheon && pkill cldm");} 
+
+        else if (decision == "unity")        {system("startx /usr/bin/unity && pkill cldm");} 
+
+        else if (decision == "deepin")       {system("startx /usr/bin/deepin && pkill cldm");} 
+
+        else if (decision == "i3")           {system("startx /usr/bin/i3 && pkill cldm");} 
+
+        else if (decision == "sway")         {system("startx /usr/bin/sway && pkill cldm");} 
+
+        else if (decision == "xmonad")       {system("startx /usr/bin/xmonad && pkill cldm");} 
+
+        else if (decision == "spectrwm")     {system("startx /usr/bin/spectrwm && pkill cldm");} 
+
+        else if (decision == "xfwm")         {system("startx /usr/bin/xfwm && pkill cldm");}
     }
+
     return 0;
+}
