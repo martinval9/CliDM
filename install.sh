e7e304a5b4d56d815553246e7ae06c8f76509913
martinval9 <68918892+martinval9@users.noreply.github.com>
2021-07-25 15:55:01 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -8,5 +8,8 @@ sudo cp verificar.py /usr/bin/
 
 sudo cp cldm.cpp .config
 
-echo 'Console Display Manager Instalado :D'
-echo 'Para lanzarlo debes entrar al tty(Ctrl+Alt+F2) de tu distro linux y escribir "cldm" sin las comillas'
+echo 'Console Display Manager Instalado :D / Console Display Manager Installed :D'
+
+echo ''
+
+echo 'Para lanzarlo debes entrar al tty(Ctrl+Alt+F2) de tu distro linux y escribir "cldm" sin las comillas / To launch it you must enter the tty (Ctrl + Alt + F2) of your linux distro and write "cldm" without the quotes'
